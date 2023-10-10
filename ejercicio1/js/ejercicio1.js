4882dfd07a6597e2b1304e3c22e57313f0353b76
Sergioig37 <sergioig37@educastur.es>
2023-10-10 10:43:06 +0200

    Ejercicio 1 terminado ( en principio)

---
+++ ejercicio1/js/ejercicio1.js
@@ -0,0 +1,48 @@
+
+function pedirFrase(){
+    
+    
+    do{
+        var frase = prompt("Escribe la palabra o frase a la que quieras dar la vuelta"+"\n" +"Escribe ESC o esc para salir del prograna ");
+        if(frase!=="esc"&&frase!=="ESC"){
+            console.log(frase);
+            girarPalabras(separarPalabras(frase, " "));
+        }
+    }
+    while(frase!=="esc"&&frase!=="ESC")
+}
+
+
+function separarPalabras(frase, separador){
+    
+    let cadena = "";
+    var palabras = [];
+    var letras = []
+     palabras=frase.split(separador);
+
+     for(var i=0;i<palabras.length;i++){
+         cadena = palabras[i];
+        for(var j=cadena.length-1;j>=0;j--){
+            
+            if(j==0){
+                letras.push(cadena.charAt(j));
+                letras.push(" ");
+            }else{
+                letras.push(cadena.charAt(j));
+            } 
+        }
+    }
+    return letras
+
+}
+
+function girarPalabras(arrayNormal){
+
+   var textoInverso = "";
+   textoInverso += arrayNormal.join("");
+    console.log(textoInverso);
+
+}
+
+
+pedirFrase();
