992c10b2898f069be40a95d4a4137a034fcf47df
Sergioig37 <sergioig37@educastur.es>
2023-10-13 11:39:07 +0200

    Ejercicio 3 TERMINADO

--- ejercicio3/js/ejercico3.js
+++ ejercicio3/js/ejercico3.js
@@ -25,27 +25,26 @@ function muestraEquipos(EQUIPOS) {
 
 function generarResultados( quiniela){
 
-        var quinielita = new Array(quiniela.length);
     
         for (let i = 0; i < quiniela.length; i++) {
-            quinielita[i] = new Array(quiniela.length);
+            quiniela[i] = new Array(quiniela.length);
             for (let j = 0; j < quiniela.length; j++) {
                 if (i === j) {
-                    quinielita[i][j] = "-";
+                    quiniela[i][j] = "-";
                 } else {
-                    quinielita[i][j] = generarSimbolo();
+                    quiniela[i][j] = generarSimbolo();
                 }
             }
         }
     
-        console.table(quinielita);
+        console.table(quiniela);
     
 }
 
 function quinielas(EQUIPOS){
 
     do{
-        var eleccion = prompt("Elige que vas a hacer" +"\n"+ "1: Generar quinielas" +"\n"+"2: Ver equipos" +"\n" +"3: Salir del programa");
+        var eleccion = prompt("Elige que vas a hacer" +"\n"+ "1: Generar quinielas" +"\n"+"2: Ver equipos" +"\n" +"3: Consultar resultados de un partido" +"\n"+ "4: Terminar programa");
         switch(eleccion){
             case("1"): 
                 generarResultados(quiniela);
@@ -53,10 +52,55 @@ function quinielas(EQUIPOS){
             case("2"): 
                 muestraEquipos(EQUIPOS);
                 break;
+            case("3"):
+                muestraDatos(EQUIPOS, quiniela);
+                break;
         }
     }
-    while(eleccion!="3");
+    while(eleccion!="4");
 
 }
+
+
+
+
+function muestraDatos(EQUIPOS, quiniela){
+    do{
+        var equipo1 = prompt("Introduce el equipo local: ");
+         equipo1 = repetirPregunta(equipo1);
+        var equipo2 = prompt("Introduce el equipo visitante");
+         equipo2 = repetirPregunta(equipo2);
+        console.log("El resultado de "+equipo1+ " contra " +equipo2 + " es " +quiniela[EQUIPOS.indexOf(equipo1)][EQUIPOS.indexOf(equipo2)]);
+        var eleccion = prompt("¿Desea realizaer una nueva consulta?"+"\n"+"SI"+"\n"+"NO");
+    }
+    while(eleccion!="NO");
+}
+
+function repetirPregunta(equipo){
+    if(comprobarExiste(equipo)==false){
+        while(comprobarExiste(equipo)==false){
+            equipo = prompt("El equipo no existe, prueba otra vez");
+        }
+    }
+   return equipo;
+}
+
+
+function comprobarExiste(equipo){
+
+    var existeEquipo = false;
+
+    for(var i=0;i<EQUIPOS.length;i++){
+        if(equipo==EQUIPOS[i]){
+            existeEquipo = true;
+            break;
+        }
+    }
+    return existeEquipo;
+}
+
+
+
+
 
 quinielas(EQUIPOS);
